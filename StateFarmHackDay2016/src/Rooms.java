37e72fea9c1a45013c32d75ceb9b646ac0cd88ee
Jason Shipp <shippj@purdue.edu>
2016-06-15 12:38:15 -0500

    Added availibilty times and method to check for it -- needs testing

--- StateFarmHackDay2016/src/Rooms.java
+++ StateFarmHackDay2016/src/Rooms.java
@@ -1,32 +1,53 @@
+import java.util.ArrayList;
 
 public class Rooms extends Floor {
 
 	
 	String roomName;
 	String availability;
-	int width, length, location;
+	int width, length, locationX, locationY;
+	boolean isAvailable;
+	ArrayList<String> taken;
+	ArrayList<String> allTimes;
 	
 	
-	
-	Rooms(String name, String campus, String floorNumber, String divisions, String roomName, String availability, int width, 
-			int length, int location) 
+	Rooms(String name, String campus, String floorNumber, String divisions, String roomName, ArrayList<String> taken, int width, 
+			int length, int locationX, int locationY) 
 	{
 		super(name, campus, floorNumber, divisions);
 		// TODO Auto-generated constructor stub
 		this.roomName = roomName;
-		this.availability = availability;
+		this.taken = taken;
 		this.width = width;
 		this.length = length;
-		this.location = length;
+		this.locationX = locationX;
+		this.locationY = locationY;
+		allTimes = new ArrayList<String>() {{
+			add("7:00");add("7:30");add("8:00");add("8:30");add("9:00");add("9:30");add("10:00");add("10:30");
+			add("11:00");add("11:30");add("12:00");add("12:30");add("1:00");add("1:30");add("2:00");
+			add("2:30");add("3:00");add("3:30");add("4:00");add("4:30");add("5:00");add("5:30");
+		}};
 	}
 
 	String getRoomName()
 	{
 		return roomName;
 	}
-	String getAvailability()
+	ArrayList<String> getAvailability()
 	{
+		ArrayList<String> availability = (ArrayList<String>)allTimes.clone();
+		for(String x: taken) {
+			availability.remove(x);
+		}
 		return availability;
+	}
+	boolean isAvailble(String time) {
+		ArrayList<String> availability = getAvailability();
+		if(availability.contains(time))
+			isAvailable = true;
+		else
+			isAvailable = false;
+		return isAvailable;
 	}
 	
 }
